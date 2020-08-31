1daaadcdafc6c45a82a0fb6cf1a016583d702496
Dinith <34013038+Dinith1@users.noreply.github.com>
2020-08-31 18:42:49 +1200

    [CPM-64] Set up CI (#6)
    * [CPM-64] set up CI
    
    * update build and test commands
    
    * comment api build and test commands
    
    Prevents build errors due to non-existent build script and auto-fail test script
    
    * add audit, remove client CI
    
    * add separate file for client CI
    
    * run build on change to api/* only
    
    * run on changes to client/* only
    
    * delete App.test.js
    
    * change pull request path
    
    * update path
    
    * update path
    
    * update name
    
    * update api ci to match client
    
    * comment out audit command
    
    * comment audit in api ci
    
    * add passWithNoTests flag
    
    * add change for testing api ci

--- api/src/index.js
+++ api/src/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3001;
 const startServer = async () => {
     const app = express();
     await loaders(app);
-    app.listen(port, () => logger.info(`Server running on port ${port}`));
+    app.listen(port, () => logger.info(`Server running on port ${port} :)`));
 };
 
 startServer();
