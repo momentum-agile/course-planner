7d82e878c2e24b0f990a82ad799a0c625ecb1161
Vanessa Ciputra <38397520+vee-christella@users.noreply.github.com>
2020-09-21 17:15:16 +1200

    [CPM-133] Update Options Menu (#65)
    * Remove "options" title
    
    * Fix misalignment of icon
    
    * Fix menu and add icons
    
    * Adjust size and position of menu
    
    * Update snapshots

--- client/src/components/OptionsMenu.js
+++ client/src/components/OptionsMenu.js
@@ -1,29 +1,40 @@
 import React from "react";
-import { Menu, MenuButton, MenuList, MenuGroup, MenuItem, Icon, Text, Flex } from "@chakra-ui/core";
-import { BsThreeDots } from "react-icons/bs";
+import { Menu, MenuButton, MenuList, MenuGroup, MenuItem, Icon, Text, Flex, IconButton } from "@chakra-ui/core";
+import { BiDotsHorizontalRounded } from "react-icons/bi";
 import ConfirmationDialog from "./ConfirmationDialog";
 
 const OptionsMenu = ({ item, itemType, setOpenConfirmationDialog, openConfirmationDialog, confirm, hasEdit, onEdit }) => {
     return (
         <Flex>
             <Menu>
-                <MenuButton>
-                    <Flex align="center" justify="center">
-                        <Icon as={BsThreeDots} size="60px" />
-                    </Flex>
-                </MenuButton>
-                <MenuList>
-                    <MenuGroup title="Options">
-                        <MenuItem onClick={() => setOpenConfirmationDialog(true)}>
-                            <Text color="red.500">Delete</Text>
-                        </MenuItem>
-                        {hasEdit && (
-                            <MenuItem onClick={onEdit}>
-                                <Text>Edit</Text>
-                            </MenuItem>
-                        )}
-                    </MenuGroup>
-                </MenuList>
+                {({ isOpen }) => (
+                    <React.Fragment>
+                        <MenuButton>
+                            <IconButton
+                                as={BiDotsHorizontalRounded}
+                                size="xs"
+                                _active={{ bg: "#063b59", color: "#F2F2F2" }}
+                                _hover={isOpen ? { bg: "#063b59", color: "#F2F2F2" } : { bg: "#D3D3D3", color: "black" }}
+                                bg={isOpen ? "#063b59" : "none"}
+                                color={isOpen ? "#F2F2F2" : "black"}
+                            />
+                        </MenuButton>
+                        <MenuList placement="bottom-end" minWidth="160px">
+                            <MenuGroup>
+                                <MenuItem onClick={() => setOpenConfirmationDialog(true)}>
+                                    <Icon name="delete" mr={3} color="red.500" />
+                                    <Text color="red.500">Delete</Text>
+                                </MenuItem>
+                                {hasEdit && (
+                                    <MenuItem onClick={onEdit}>
+                                        <Icon name="edit" mr={3} />
+                                        <Text>Edit</Text>
+                                    </MenuItem>
+                                )}
+                            </MenuGroup>
+                        </MenuList>
+                    </React.Fragment>
+                )}
             </Menu>
             <ConfirmationDialog
                 item={item}
