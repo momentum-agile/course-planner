530d82aadf2861c7c73a494a7af98bebb9004cf4
Richard Ng <42757966+Richardng12@users.noreply.github.com>
2020-09-23 16:42:21 +1200

    [CPM-149] Search Bar Overlap Fix (#77)
    * changed z-index so overlapping doesnt occur
    
    * update snapshot tests

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -10,7 +10,7 @@ const SearchBar = ({ searchCategory, onChange, value }) => {
         <Flex height="40px" width="100%" direction="row" justify="center" marginTop="10px">
             <Flex width="80%" backgroundColor={c.lightGrey} align="center">
                 <Flex width="100%" justify="center" align="center">
-                    <InputGroup width="100%">
+                    <InputGroup zIndex={0} width="100%">
                         <InputLeftElement children={<Icon name="search" color="gray.300" />} />
                         <Input type="search" placeholder={placeholderText} onChange={onChange} value={value} />
                     </InputGroup>
