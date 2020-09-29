9c86110aa983a7d11b34fa4519c80f53a09f4505
daltonlim <39323194+daltonlim@users.noreply.github.com>
2020-09-29 13:31:48 +1300

    [CPM-170] Enable autoscroll to a given course (#106)
    * Enable autoscroll to a given course
    
    * delete scroll to bottom
    
    * snap update

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -7,15 +7,8 @@ const Table = ({ columns, data, getRowProps = () => ({}), currRow, searchInput, 
     const [dataLength, setDataLength] = useState(null);
     const endRef = useRef(null);
 
-    const scrollToBottom = () => {
-        endRef.current.scrollIntoView({ behavior: "smooth" });
-    }
-
     useEffect(() => {
-        if (dataLength && dataLength < data.length) {
-            scrollToBottom();
-        }
-
+        endRef && endRef.current && endRef.current.scrollIntoView({ behavior: "smooth" });
         setDataLength(data.length);
     }, [data, dataLength]);
 
@@ -34,7 +27,6 @@ const Table = ({ columns, data, getRowProps = () => ({}), currRow, searchInput, 
     return (
         <Box height="75vh" overflowY="scroll" width="100%" className="table">
             <Box as="table" width="100%" {...getTableProps()}>
-
                 <Box as="thead">
                     {headerGroups.map((headerGroup, idx) => (
                         <TableHeader key={idx} headerGroup={headerGroup} />
@@ -45,18 +37,26 @@ const Table = ({ columns, data, getRowProps = () => ({}), currRow, searchInput, 
                     {rows.map((row, idx) => {
                         prepareRow(row);
                         return (
-                            <TableRow key={idx} row={row} currRow={currRow} getRowProps={getRowProps} rowHover={rowHover} />
+                            <TableRow
+                                ref={row.id === currRow ? endRef : null}
+                                key={idx}
+                                row={row}
+                                currRow={currRow}
+                                getRowProps={getRowProps}
+                                rowHover={rowHover}
+                            />
                         );
                     })}
-                    <div ref={endRef} />
                 </Box>
-
             </Box>
 
-            {rows.length === 0 &&
+            {rows.length === 0 && (
                 <Box align="center" justify="center" width="100%" mt="20%">
-                    <Text fontStyle="italic" color={c.white} textAlign="center">No data found</Text>
-                </Box>}
+                    <Text fontStyle="italic" color={c.white} textAlign="center">
+                        No data found
+                    </Text>
+                </Box>
+            )}
         </Box>
     );
 };
@@ -64,41 +64,24 @@ const Table = ({ columns, data, getRowProps = () => ({}), currRow, searchInput, 
 const TableHeader = ({ headerGroup }) => {
     return (
         <Box as="tr" {...headerGroup.getHeaderGroupProps()} color={c.lightBlue}>
-            {headerGroup.headers.map(column => (
-                <Box
-                    as="th"
-                    m={0}
-                    p="0.5rem"
-                    {...column.getHeaderProps()}
-                >
-                    <Text textAlign="left">
-                        {column.render("Header")}
-                    </Text>
+            {headerGroup.headers.map((column) => (
+                <Box as="th" m={0} p="0.5rem" {...column.getHeaderProps()}>
+                    <Text textAlign="left">{column.render("Header")}</Text>
                 </Box>
             ))}
         </Box>
     );
-}
-
-const TableRow = ({ row, getRowProps, rowHover }) => {
-    return (
-        <PseudoBox as="tr" {...row.getRowProps(getRowProps(row))} _hover={rowHover}>
-            {row.cells.map((cell) => {
-                return (
-                    <Box
-                        as="td"
-                        m={0}
-                        p="0.5rem"
-                        {...cell.getCellProps()}
-                    >
-                        <Text>
-                            {cell.render("Cell")}
-                        </Text>
-                    </Box>
-                );
-            })}
-        </PseudoBox>
-    );
-}
+};
+const TableRow = React.forwardRef(({ row, getRowProps, rowHover }, ref) => (
+    <PseudoBox as="tr" ref={ref} {...row.getRowProps(getRowProps(row))} _hover={rowHover}>
+        {row.cells.map((cell) => {
+            return (
+                <Box as="td" m={0} p="0.5rem" {...cell.getCellProps()}>
+                    <Text>{cell.render("Cell")}</Text>
+                </Box>
+            );
+        })}
+    </PseudoBox>
+));
 
 export default Table;
