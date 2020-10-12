f677b81506815e753159cad0b4cbbcdc40c4f516
Vanessa Ciputra <38397520+vee-christella@users.noreply.github.com>
2020-10-12 13:01:45 +1300

    [CPM-178] Change UoA Logo + Cleanup (#127)
    * Logo swap + cleanup of public resources
    
    * Update snapshots
    
    * Update alt text
    
    * Snapshots

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -6,16 +6,17 @@ import { colors as c } from "../../colors";
 const Home = () => {
     return (
         <Flex height="100vh" width="100%" direction="row">
-            <Flex width="70%" backgroundColor={c.iceBlue} direction="column">
+            <Flex width="70%" backgroundColor={c.iceBlue} direction="column" justify="center">
                 <Flex width="100%" height="100%" align="center" justify="center">
                     <Image
                         htmlWidth="1200px"
                         htmlHeight="600px"
                         objectFit="cover"
-                        src="./LabelledTile.png"
-                        alt="Homepage Image of Course Planner's screeshots"
+                        src="./Tile.png"
+                        alt="Homepage image of Calendar/Tile Placer from Canva"
                     />
                 </Flex>
+                <Image src="./momentum_logo.png" alt="Team Momentum Logo" width="10%" mb={2} ml={2} />
             </Flex>
             <Flex width="30%" backgroundColor={c.white}>
                 <Flex width="100%" direction="column" marginTop="60px">
@@ -36,7 +37,7 @@ const Home = () => {
                         </Flex>
                     </Flex>
                     <Flex align="flex-end" justify="center" height="100%">
-                        <Image src="./momentum_logo.png" alt="UoA Logo" width="30%" mb={5} />
+                        <Image width="80%" objectFit="cover" src="./UoALogo.png" alt="University of Auckland Logo" mb={5} />
                     </Flex>
                 </Flex>
             </Flex>
